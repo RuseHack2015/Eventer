af52747c968c10470840152fdea01f8bad0f93f2
Simeon Iliev <sisabski23@gmail.com>
2015-05-30 00:45:01 +0300

    Android update

--- Android/Eventer/app/src/main/java/com/example/simeon/eventer/WelcomeScreen.java
+++ Android/Eventer/app/src/main/java/com/example/simeon/eventer/WelcomeScreen.java
@@ -1,17 +1,30 @@
 package com.example.simeon.eventer;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class WelcomeScreen extends ActionBarActivity {
+
+    private Button cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        cameraButton = (Button) findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(WelcomeScreen.this, CameraActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
