390b60b410fe3c29521b50c0cc30fc60b40321d4
indjev99 <emil.indjev@gmail.com>
2015-05-30 10:55:47 +0300

    Test

--- Java/src/com/eventer/objectControllers/UserController.java
+++ Java/src/com/eventer/objectControllers/UserController.java
@@ -7,7 +7,7 @@ import com.eventer.objects.Event;
 import com.eventer.objects.User;
 
 public class UserController {
-	public void attendEvent(String user, String event) {/*
+	public void attendEvent(String user, String event) {
 		User curUser = getUserFromDB(user);
 		Event curEvent = getEventFromDB(event);
 		curUser.visitEvent(event);
@@ -15,7 +15,8 @@ public class UserController {
 		List<String> tags = curEvent.getTags();
 		for (int i = 0; i < tags.size(); i++) {
 			curUser.visitTag(tags.get(i));
+		setUserToDB(curUser);
 		
-		}*/
+		}
 	}
 }
