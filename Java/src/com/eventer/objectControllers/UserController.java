d24b82641964156db5a7cb901943f9b09c37c85b
indjev99 <emil.indjev@gmail.com>
2015-05-30 10:58:37 +0300

    Changed UserController.

--- Java/src/com/eventer/objectControllers/UserController.java
+++ Java/src/com/eventer/objectControllers/UserController.java
@@ -15,8 +15,7 @@ public class UserController {
 		List<String> tags = curEvent.getTags();
 		for (int i = 0; i < tags.size(); i++) {
 			curUser.visitTag(tags.get(i));
-		setUserToDB(curUser);
-		
 		}
+		setUserToDB(curUser);
 	}
 }
